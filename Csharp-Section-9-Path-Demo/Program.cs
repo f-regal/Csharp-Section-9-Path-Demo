e5a4c707cadf152e1854f09af97f004858658b73
Faysal Regal <faysalregal@gmail.com>
2022-08-29 18:24:47 +0100

    used the Path class

--- Csharp-Section-9-Path-Demo/Program.cs
+++ Csharp-Section-9-Path-Demo/Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
-
+using System.IO;
 namespace CsharpSection9PathDemo
 {
     class MainClass
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Wrld!");
+            var path = @"/Users/faysalregal/Desktop/C# Fundamentals/Section 9 Working with Files/Demo/Csharp-Section-9-Directory-Demo/Csharp-Section-9-Directory-Demo.sln";
+
+            //Old way
+            var dotIndex = path.IndexOf('.');
+            var extension = path.Substring(dotIndex);
+
+            //Convenient Way
+            Console.WriteLine("Extension: " + Path.GetExtension(path));
+            Console.WriteLine("File Name: " + Path.GetFileName(path));
+            Console.WriteLine("File Name without Extension: " + Path.GetFileNameWithoutExtension(path));
+            Console.WriteLine("Directory: " + Path.GetDirectoryName(path));
         }
     }
 }
