488f6e526a1ab4a2e9e3fabcd14ea4404f157e82
Faysal Regal <faysalregal@gmail.com>
2022-08-29 18:15:28 +0100

    first commit

---
+++ Csharp-Section-9-Path-Demo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CsharpSection9PathDemo
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
